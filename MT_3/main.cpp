5df381da0fa7e00cb845709d05f358d59e065165
GC1C_16_トミタ_アヤナ <k022g1021@g.neec.ac.jp>
2023-05-09 17:38:35 +0900

    00_02/完成

--- MT_3/main.cpp
+++ MT_3/main.cpp
@@ -99,126 +99,127 @@ Matrix4x4 Inverse(const Matrix4x4& m1)
 		- m1.m[1][1] * m1.m[2][3] * m1.m[3][2])
 		* deteminantRecp };
 	result.m[0][1] = { -(
-		  m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][])
+		  m1.m[0][1] * m1.m[2][2] * m1.m[3][3]
+		+ m1.m[0][2] * m1.m[2][3] * m1.m[3][1]
+		+ m1.m[0][3] * m1.m[2][1] * m1.m[3][2]
+		- m1.m[0][3] * m1.m[2][2] * m1.m[3][1]
+		- m1.m[0][2] * m1.m[2][1] * m1.m[3][3]
+		- m1.m[0][1] * m1.m[2][3] * m1.m[3][2])
 		* deteminantRecp };
 	result.m[0][2] = { (
-		  m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][])
+		  m1.m[0][1] * m1.m[1][2] * m1.m[3][3]
+		+ m1.m[0][2] * m1.m[1][3] * m1.m[3][1]
+		+ m1.m[0][3] * m1.m[1][1] * m1.m[3][2]
+		- m1.m[0][3] * m1.m[1][2] * m1.m[3][1]
+		- m1.m[0][2] * m1.m[1][1] * m1.m[3][3]
+		- m1.m[0][1] * m1.m[1][3] * m1.m[3][2])
 		* deteminantRecp };
 	result.m[0][3] = { -(
-		  m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][])
+		  m1.m[0][1] * m1.m[1][2] * m1.m[2][3]
+		+ m1.m[0][2] * m1.m[1][3] * m1.m[2][1]
+		+ m1.m[0][3] * m1.m[1][1] * m1.m[2][2]
+		- m1.m[0][3] * m1.m[1][2] * m1.m[2][1]
+		- m1.m[0][2] * m1.m[1][1] * m1.m[2][3]
+		- m1.m[0][1] * m1.m[1][3] * m1.m[2][2])
 		* deteminantRecp };
 	result.m[1][0] = { -(
-		  m1.m[1][] * m1.m[2][] * m1.m[2][]
-		+ m1.m[1][] * m1.m[2][] * m1.m[2][]
-		+ m1.m[1][] * m1.m[2][] * m1.m[2][]
-		- m1.m[1][] * m1.m[2][] * m1.m[2][]
-		- m1.m[1][] * m1.m[2][] * m1.m[2][]
-		- m1.m[1][] * m1.m[2][] * m1.m[2][])
+		  m1.m[1][0] * m1.m[2][2] * m1.m[2][3]
+		+ m1.m[1][2] * m1.m[2][3] * m1.m[2][0]
+		+ m1.m[1][3] * m1.m[2][0] * m1.m[2][2]
+		- m1.m[1][3] * m1.m[2][2] * m1.m[2][0]
+		- m1.m[1][2] * m1.m[2][0] * m1.m[2][3]
+		- m1.m[1][0] * m1.m[2][3] * m1.m[2][2])
 		* deteminantRecp };
 	result.m[1][1] = { (
-		  m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][])
+		  m1.m[0][0] * m1.m[2][2] * m1.m[3][3]
+		+ m1.m[0][2] * m1.m[2][3] * m1.m[3][0]
+		+ m1.m[0][3] * m1.m[2][0] * m1.m[3][2]
+		- m1.m[0][3] * m1.m[2][2] * m1.m[3][0]
+		- m1.m[0][2] * m1.m[2][0] * m1.m[3][3]
+		- m1.m[0][0] * m1.m[2][3] * m1.m[3][2])
 		* deteminantRecp };
 	result.m[1][2] = { -(
-		  m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][])
+		  m1.m[0][0] * m1.m[1][2] * m1.m[3][3]
+		+ m1.m[0][2] * m1.m[1][3] * m1.m[3][0]
+		+ m1.m[0][3] * m1.m[1][0] * m1.m[3][2]
+		- m1.m[0][3] * m1.m[1][2] * m1.m[3][0]
+		- m1.m[0][2] * m1.m[1][0] * m1.m[3][3]
+		- m1.m[0][0] * m1.m[1][3] * m1.m[3][2])
 		* deteminantRecp };
 	result.m[1][3] = { (
-		  m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][])
+		  m1.m[0][0] * m1.m[1][2] * m1.m[2][3]
+		+ m1.m[0][2] * m1.m[1][3] * m1.m[2][0]
+		+ m1.m[0][3] * m1.m[1][0] * m1.m[2][2]
+		- m1.m[0][3] * m1.m[1][2] * m1.m[2][0]
+		- m1.m[0][2] * m1.m[1][0] * m1.m[2][3]
+		- m1.m[0][0] * m1.m[1][3] * m1.m[2][2])
 		* deteminantRecp };
 	result.m[2][0] = { (
-		  m1.m[1][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[1][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[1][] * m1.m[2][] * m1.m[3][]
-		- m1.m[1][] * m1.m[2][] * m1.m[3][]
-		- m1.m[1][] * m1.m[2][] * m1.m[3][]
-		- m1.m[1][] * m1.m[2][] * m1.m[3][])
+		  m1.m[1][0] * m1.m[2][1] * m1.m[3][3]
+		+ m1.m[1][1] * m1.m[2][3] * m1.m[3][0]
+		+ m1.m[1][3] * m1.m[2][0] * m1.m[3][1]
+		- m1.m[1][3] * m1.m[2][1] * m1.m[3][0]
+		- m1.m[1][1] * m1.m[2][0] * m1.m[3][3]
+		- m1.m[1][0] * m1.m[2][3] * m1.m[3][1])
 		* deteminantRecp };
 	result.m[2][1] = { -(
-		  m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][])
+		  m1.m[0][0] * m1.m[2][1] * m1.m[3][3]
+		+ m1.m[0][1] * m1.m[2][3] * m1.m[3][0]
+		+ m1.m[0][3] * m1.m[2][0] * m1.m[3][1]
+		- m1.m[0][3] * m1.m[2][1] * m1.m[3][0]
+		- m1.m[0][1] * m1.m[2][0] * m1.m[3][3]
+		- m1.m[0][0] * m1.m[2][3] * m1.m[3][1])
 		* deteminantRecp };
 	result.m[2][2] = { (
-		  m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][])
+		  m1.m[0][0] * m1.m[1][1] * m1.m[3][3]
+		+ m1.m[0][1] * m1.m[1][3] * m1.m[3][0]
+		+ m1.m[0][3] * m1.m[1][0] * m1.m[3][1]
+		- m1.m[0][3] * m1.m[1][1] * m1.m[3][0]
+		- m1.m[0][1] * m1.m[1][0] * m1.m[3][3]
+		- m1.m[0][0] * m1.m[1][3] * m1.m[3][1])
 		* deteminantRecp };
 	result.m[2][3] = { -(
-		  m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][])
+		  m1.m[0][0] * m1.m[1][1] * m1.m[2][3]
+		+ m1.m[0][1] * m1.m[1][3] * m1.m[2][0]
+		+ m1.m[0][3] * m1.m[1][0] * m1.m[2][1]
+		- m1.m[0][3] * m1.m[1][1] * m1.m[2][0]
+		- m1.m[0][1] * m1.m[1][0] * m1.m[2][3]
+		- m1.m[0][0] * m1.m[1][3] * m1.m[2][1])
 		* deteminantRecp };
 	result.m[3][0] = { -(
-		  m1.m[1][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[1][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[1][] * m1.m[2][] * m1.m[3][]
-		- m1.m[1][] * m1.m[2][] * m1.m[3][]
-		- m1.m[1][] * m1.m[2][] * m1.m[3][]
-		- m1.m[1][] * m1.m[2][] * m1.m[3][])
+		  m1.m[1][0] * m1.m[2][1] * m1.m[3][2]
+		+ m1.m[1][1] * m1.m[2][2] * m1.m[3][0]
+		+ m1.m[1][2] * m1.m[2][0] * m1.m[3][1]
+		- m1.m[1][2] * m1.m[2][1] * m1.m[3][0]
+		- m1.m[1][1] * m1.m[2][0] * m1.m[3][2]
+		- m1.m[1][0] * m1.m[2][2] * m1.m[3][1])
 		* deteminantRecp };
 	result.m[3][1] = { (
-		  m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][]
-		- m1.m[0][] * m1.m[2][] * m1.m[3][])
+		  m1.m[0][0] * m1.m[2][1] * m1.m[3][2]
+		+ m1.m[0][1] * m1.m[2][2] * m1.m[3][0]
+		+ m1.m[0][2] * m1.m[2][0] * m1.m[3][1]
+		- m1.m[0][2] * m1.m[2][1] * m1.m[3][0]
+		- m1.m[0][1] * m1.m[2][0] * m1.m[3][2]
+		- m1.m[0][0] * m1.m[2][2] * m1.m[3][1])
 		* deteminantRecp };
 	result.m[3][2] = { -(
-		  m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][]
-		- m1.m[0][] * m1.m[1][] * m1.m[3][])
+		  m1.m[0][0] * m1.m[1][1] * m1.m[3][2]
+		+ m1.m[0][1] * m1.m[1][2] * m1.m[3][0]
+		+ m1.m[0][2] * m1.m[1][0] * m1.m[3][1]
+		- m1.m[0][2] * m1.m[1][1] * m1.m[3][0]
+		- m1.m[0][1] * m1.m[1][0] * m1.m[3][2]
+		- m1.m[0][0] * m1.m[1][2] * m1.m[3][1])
 		* deteminantRecp };
 	result.m[3][3] = { (
-		  m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		+ m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][]
-		- m1.m[0][] * m1.m[1][] * m1.m[2][])
+		  m1.m[0][0] * m1.m[1][1] * m1.m[2][2]
+		+ m1.m[0][1] * m1.m[1][2] * m1.m[2][0]
+		+ m1.m[0][2] * m1.m[1][0] * m1.m[2][1]
+		- m1.m[0][2] * m1.m[1][1] * m1.m[2][0]
+		- m1.m[0][1] * m1.m[1][0] * m1.m[2][2]
+		- m1.m[0][0] * m1.m[1][2] * m1.m[2][1])
 		* deteminantRecp };
 
+	return result;
 }
 //転置行列
 Matrix4x4 Transpose(const Matrix4x4& m1)
@@ -236,7 +237,43 @@ Matrix4x4 Transpose(const Matrix4x4& m1)
 	return result;
 }
 //単位行列の作成
-Matrix4x4 MakeIdentity4x4() {}
+Matrix4x4 MakeIdentity4x4() {
+	Matrix4x4 result;
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (i == j)
+			{
+				result.m[i][j] = 1;
+			}
+			else
+			{
+				result.m[i][j] = 0;
+			}
+		}
+	}
+
+	return result;
+}
+
+//Matrix4x4の数値表示
+Vector2 textWH = { 60,20 };
+void MatrixScreenPrinsf(const Vector2& pos, const Matrix4x4& m, const char* label)
+{
+	Novice::ScreenPrintf((int)pos.x, (int)pos.y, "%s", label);
+	for (int i = 0; i < 4; ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			Novice::ScreenPrintf(
+				(int)(pos.x + j * textWH.x), (int)(pos.y + (i + 1) * textWH.y),
+				"%6.02f", m.m[i][j]
+			);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -247,6 +284,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	Matrix4x4 m1 = {
+		3.2f,0.7f,9.6f,4.4f,
+		5.5f,1.3f,7.8f,2.1f,
+		6.9f,8.0f,2.6f,1.0f,
+		0.5f,7.2f,5.1f,3.3f
+	};
+	Matrix4x4 m2 = {
+		4.1f,6.5f,3.3f,2.2f,
+		8.8f,0.6f,9.9f,7.7f,
+		1.1f,5.5f,6.6f,0.0f,
+		3.3f,9.9f,8.8f,2.2f
+	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -261,6 +311,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 resultAdd = Add(m1, m2);
+		Matrix4x4 resultMultiply = Multiply(m1, m2);
+		Matrix4x4 resultSubtract = Subtract(m1, m2);
+		Matrix4x4 inverseM1 = Inverse(m1);
+		Matrix4x4 inverseM2 = Inverse(m2);
+		Matrix4x4 transposeM1 = Transpose(m1);
+		Matrix4x4 transposeM2 = Transpose(m2);
+		Matrix4x4 identity = MakeIdentity4x4();
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -268,6 +327,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrinsf({ 0,0 }, resultAdd, "Add");
+		MatrixScreenPrinsf({ 0,textWH.y * 5 }, resultSubtract, "Subtract");
+		MatrixScreenPrinsf({ 0,textWH.y * 5 * 2 }, resultMultiply, "Multiply");
+		MatrixScreenPrinsf({ 0,textWH.y * 5 * 3 }, inverseM1, "InverseM1");
+		MatrixScreenPrinsf({ 0,textWH.y * 5 * 4 }, inverseM2, "InverseM2");
+		MatrixScreenPrinsf({ textWH.x * 5,0 }, transposeM1, "TransposeM1");
+		MatrixScreenPrinsf({ textWH.x * 5,textWH.y * 5 }, transposeM2, "TransposeM2");
+		MatrixScreenPrinsf({ textWH.x * 5,textWH.y * 5 * 2 }, identity, "Identity");
 
 		///
 		/// ↑描画処理ここまで
